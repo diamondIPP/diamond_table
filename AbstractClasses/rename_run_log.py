36e81b4f3d8f03501dd482e4ca2b3c4b7f7483d6
micha <micha.reichmann@gmail.com>
2016-07-24 01:09:28 +0200

    sorting dict by runnumber

--- AbstractClasses/rename_run_log.py
+++ AbstractClasses/rename_run_log.py
@@ -2,11 +2,14 @@
 
 from sys import argv
 from json import load, dump
+from datetime import datetime
+from collections import OrderedDict
 
 path = argv[1] if len(argv) > 1 else ''
 f = open(path, 'r+')
 runinfo = load(f)
-new_runinfo = {}
+new_runinfo = OrderedDict()
+
 
 def find_for_in_comment(info, new_info):
     for name in ['for1', 'for2']:
@@ -17,8 +20,9 @@ def find_for_in_comment(info, new_info):
                 if str(cmt[0].lower()) == name:
                     new_info[name] = int(cmt[1])
 
-for key, item in runinfo.iteritems():
-    new_key = str(key.split('00')[-1])
+runinfo = {int(key): value for key, value in runinfo.iteritems()}
+for key, item in sorted(runinfo.iteritems()):
+    new_key = str(key)
     if new_key:
         new_runinfo[new_key] = runinfo[key]
     find_for_in_comment(item, new_runinfo[new_key])
@@ -32,6 +36,6 @@ for key, item in runinfo.iteritems():
     except KeyError:
         pass
 f.seek(0)
-dump(new_runinfo, f, indent=2, sort_keys=True)
+dump(new_runinfo, f, indent=2)
 f.truncate()
 f.close()
