619a36bd75b1fb89f282bd2af14f283b2149390e
michareichmann <micha.reichmann@gmail.com>
2016-09-21 09:44:44 +0200

    adding some output

--- copy_files.py
+++ copy_files.py
@@ -6,4 +6,6 @@ from sys import argv
 z = DiamondTable()
 copy_all = False if len(argv) <= 1 else argv[1]
 rp = argv[2] if len(argv) > 2 else None
+print 'copy all: {0}'.format('TRUE' if copy_all else 'FALSE')
+print 'run plan:', rp
 z.copy_pics(copy_all, rp)
