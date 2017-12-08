64145a80e2dca3b3b4b49d03f728c7ea5ffa8d55
Michael Reichmann <reichmann@diamond.ethz.ch>
2017-12-08 20:05:49 +0100

    adding several methods to ret biases and diamond names for a single run plan and the number of diamonds

--- src/DiamondRateScans.py
+++ src/DiamondRateScans.py
@@ -3,7 +3,6 @@
 # created on June 24th 2016 by M. Reichmann
 # --------------------------------------------------------
 
-from ConfigParser import NoOptionError
 from argparse import ArgumentParser
 from collections import OrderedDict
 from Utils import *
@@ -120,7 +119,7 @@ class DiaScans:
 
     def get_biases(self, rp, tc, ch):
         runs = self.get_runs(rp, tc)
-        return sorted(list(set([self.RunInfos[tc][str(run)]['dia{c}hv'.format(c=ch)] for run in runs])), key=abs)
+        return sorted(list(set([int(self.RunInfos[tc][str(run)]['dia{c}hv'.format(c=ch)]) for run in runs])), key=abs)
 
     def get_diamonds(self, single_tc=None):
         dias = []
@@ -135,6 +134,21 @@ class DiaScans:
                     if all(dia0 == self.load_diamond(self.RunInfos[tc][str(run)]['dia{0}'.format(ch)]) for run in runs):
                         dias.append(dia0)
         return set(dias)
+
+    def get_rp_diamonds(self, tc, rp):
+        first_run = self.RunPlans[tc][rp]['runs'][0]
+        keys = sorted([key for key in self.RunInfos[tc][str(first_run)] if key.startswith('dia') and len(key) < 6])
+        return [self.translate_dia(self.RunInfos[tc][str(first_run)][key]) for key in keys]
+
+    def get_rp_biases(self, tc, rp):
+        return [self.get_biases(rp, tc, ch) for ch in xrange(1, self.get_n_diamonds(tc, rp) + 1)]
+
+    def get_n_diamonds(self, tc, rp):
+        first_run = self.RunPlans[tc][rp]['runs'][0]
+        return sum(1 for key in self.RunInfos[tc][str(first_run)] if key.startswith('dia') and len(key) < 6)
+
+    def translate_dia(self, dia):
+        return self.Parser.get('ALIASES', dia.lower())
 
 
 if __name__ == '__main__':
