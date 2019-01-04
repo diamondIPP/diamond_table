6d6a8a5fcfc332b1747b242a7b9f9d835cf54d50
Michael <remichae@phys.ethz.ch>
2019-01-04 08:53:32 +0100

    add get_diamonds method

--- src/OldTable.py
+++ src/OldTable.py
@@ -5,9 +5,23 @@
 # --------------------------------------------------------
 
 from Table import Table
+from Utils import *
+from glob import glob
+from os.path import basename
 
 
 class OldTable(Table):
 
     def __init__(self):
         Table.__init__(self)
+        self.DataPath = join(self.DataPath, 'OLD')
+        self.Diamonds = self.get_diamonds()
+
+    def get_diamonds(self):
+        return [basename(name) for name in glob(join(self.DataPath, '*')) if basename(name) not in self.Exclude and 'index' not in name]
+
+
+
+if __name__ == '__main__':
+    z = OldTable()
+
