d57e95bda54dcccfd14aa2be22c7ad1bb176a12f
Michael <remichae@phys.ethz.ch>
2019-01-04 08:34:25 +0100

    add OldTable module

---
+++ src/OldTable.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# --------------------------------------------------------
+#       class to create the table for the old diamonds
+# created on January 4th 2019 by M. Reichmann (remichae@phys.ethz.ch)
+# --------------------------------------------------------
+
+from Table import Table
+
+
+class OldTable(Table):
+
+    def __init__(self):
+        Table.__init__(self)
