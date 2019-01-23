55b802077eb6ae4ed569a6725cc1f18c7d68fdf5
Michael <remichae@phys.ethz.ch>
2019-01-23 15:12:13 +0100

    remove redundant method and make warning if dia not in irrad file

--- src/Table.py
+++ src/Table.py
@@ -9,7 +9,6 @@ from Utils import *
 from DiamondRateScans import DiaScans
 from shutil import copy
 from glob import glob
-from numpy import mean
 from os.path import dirname, realpath, join, basename
 from progressbar import Bar, ETA, FileTransferSpeed, Percentage, ProgressBar
 from ConfigParser import NoSectionError, NoOptionError
@@ -92,66 +91,26 @@ class Table:
         if not file_exists(file_path) and len(glob(join(dirname(file_path), '*'))) > 1:
             copy(join(self.Dir, basename(file_path)), file_path)
 
-    def calc_flux(self, info):
-        if 'for1' not in info or info['for1'] == 0:
-            if 'measuredflux' in info:
-                return str('{0:5.0f}'.format(info['measuredflux'] * 2.48))
-        pixel_size = 0.01 * 0.015
-        area = [52 * 80 * pixel_size] * 2
-        file_name = basename(info['maskfile']).strip('"')
-        try:
-            if file_name.lower() in ['none.msk', 'none', 'no mask', 'pump.msk']:
-                raise UserWarning
-            f = open(join(self.Dir, 'masks', file_name), 'r')
-            data = {}
-            for line in f:
-                if line.startswith('#'):
-                    continue
-                if len(line) > 3:
-                    line = line.split()
-                    roc = int(line[1])
-                    if roc not in data:
-                        data[roc] = {}
-                    data[roc][line[0]] = (int(line[2]), int(line[3]))
-            f.close()
-            try:
-                area = [(dic['cornTop'][1] - dic['cornBot'][1] + 1) * (dic['cornTop'][0] - dic['cornBot'][0] + 1) * pixel_size for dic in data.itervalues()]
-            except KeyError:
-                area = [dic['col'][1] - dic['col'][0] + 1 * dic['row'][1] - dic['row'][0] + 1 * pixel_size for dic in data.itervalues()]
-        except IOError:
-            log_warning('Could not find mask file "{f}"! Not taking any mask!'.format(f=file_name))
-        except UserWarning:
-            pass
-        flux = [info['for{0}'.format(i + 1)] / area[i] / 1000. for i in xrange(len(area))]
-        return str('{0:5.0f}'.format(mean(flux)))
-
-    @staticmethod
-    def get_runtype(info):
-        data = info['runtype']
-        if 'signal' in data:
-            return 'signal'
-        elif 'pedes' in data:
-            return 'pedestal'
-        else:
-            return data[:10]
-
     def translate_dia(self, diamond):
         self.DiaScans.translate_dia(diamond)
 
     def get_info(self, diamond, section, option, quiet=False):
-        info = ConfigParser()
-        info.read(join(self.DataDir, diamond, 'info.ini'))
+        parser = ConfigParser()
+        parser.read(join(self.DataDir, diamond, 'info.ini'))
         try:
-            return info.get(section, option)
+            return parser.get(section, option)
         except NoOptionError:
-            log_warning('option {o} not in {d} config'.format(o=option, d=diamond)) if option == 'type' else do_nothing()
+            warning('option {o} not in {d} config'.format(o=option, d=diamond)) if option == 'type' else do_nothing()
             return ''
         except NoSectionError:
-            log_warning('section {s} not in {d} config'.format(s=section, d=diamond)) if option == 'type' and not quiet else do_nothing()
+            warning('section {s} not in {d} config'.format(s=section, d=diamond)) if option == 'type' and not quiet else do_nothing()
             return ''
 
     def get_irradiation(self, campaign, diamond):
-        return center_txt(make_irr_string(self.Irradiations[campaign][diamond]))
+        try:
+            return center_txt(make_irr_string(self.Irradiations[campaign][diamond]))
+        except KeyError:
+            warning('{} does not exist in irradiation file of {}!'.format(diamond, tc_to_str(campaign, short=False)))
 
 
 if __name__ == '__main__':
