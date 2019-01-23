77235779f2bafda4792e634ff8c3600e6314afcc
Michael <remichae@phys.ethz.ch>
2019-01-23 15:08:35 +0100

    add Events col and get_run_events()

--- src/RunTable.py
+++ src/RunTable.py
@@ -39,6 +39,7 @@ class RunTable(Table):
                   '#rs2#Hit<br>Map',
                   '#cs5#Signal',
                   '#cs4#Pulser',
+                  '#rs2#Events',
                   '#rs2#Start Time',
                   '#rs2#Duration',
                   '#rs2#Comments']
@@ -49,14 +50,14 @@ class RunTable(Table):
         rows = [[center_txt(txt) for txt in ['Distr.', '2DMap', 'Pulse Height [au]', 'Pedestal [au]', 'Noise [1&sigma;]', 'Pulse Height [au]', 'Sigma',
                                              'Pedestal [au]', 'Noise [1&sigma;]']]]
         for run in dc.Runs:
-            info = dc.RunInfos[str(run)]
+            run_info = dc.RunInfos[str(run)]
             run_path = join(dirname(dc.Path), str(run))
             create_dir(join(self.Dir, run_path))
             self.copy_index_php(run_path)
             row = [make_abs_link(join(run_path, 'index.php'), run)]                                             # Run
             row += [dc.Type]                                                                                    # Type
             row += [right_txt(make_bias_str(dc.get_run_bias(run)))]                                             # Bias
-            row += [center_txt(self.calc_flux(info))]                                                           # Flux
+            row += [center_txt(dc.get_run_flux(run))]                                                           # Flux
             row += make_pic_link('HitMap', 'Plot', use_name=False)                                              # Hit Map
             row += make_pic_link('SignalDistribution', 'Plot', use_name=False)                                  # Distribution
             row += make_pic_link('SignalMap2D', 'Plot', use_name=False)                                         # Signal Map
@@ -67,31 +68,34 @@ class RunTable(Table):
             row += make_pic_link('PulserDistributionFit', dc.get_run_pul(run, sigma=True))                      # Pulser Sigma
             row += make_pic_link('PedestalDistributionFitPulserBeamOn', dc.get_run_ped(run, pulser=True))       # Pulser Pedestal
             row += make_pic_link('PedestalDistributionFitPulserBeamOn', dc.get_run_noise(run, pulser=True))     # Pulser Pedestal Noise
-            row += [conv_time(info['starttime0']), dc.calc_run_duration(run), info['comments']]                 # Start, Duration, Comments
+            row += [center_txt(dc.get_run_events(run))]                                                         # Events
+            row += [conv_time(run_info['starttime0']), dc.calc_run_duration(run), run_info['comments']]         # Start, Duration, Comments
             rows.append(row)
         return add_bkg(HTMLTable.table(rows, header_row=header), color=self.BkgCol)
 
     def build_tc(self, tc):
 
-        info = self.DiaScans.RunInfos[tc]
-        max_channels = get_max_channels(info)
+        run_info = self.DiaScans.RunInfos[tc]
+        max_channels = get_max_channels(run_info)
         header = ['#rs2#Run',
                   '#rs2#Type',
                   '#rs2#Flux<br>[kHz/cm{0}]'.format(sup(2)),
                   '#rs2#FS11',
                   '#rs2#FSH13',
+                  '#rs2#Events',
                   '#rs2#Start Time',
                   '#rs2#Duration']
         header += ['#cs2#{}'.format(txt) for txt in (['Front', 'Middle', 'Back'] if max_channels == 3 else ['Front', 'Back'])]
         header += ['#rs2#Comments']
 
         rows = [[center_txt(txt) for txt in ['Dia', 'HV [V]'] * max_channels]]
-        for run, data in sorted(info.iteritems(), key=lambda (key, v): (int(key), v)):
+        for run, data in sorted(run_info.iteritems(), key=lambda (key, v): (int(key), v)):
             row = [center_txt(run)]
             row += [center_txt(data['runtype'])]
-            row += [center_txt(self.calc_flux(data))]
+            row += [center_txt(calc_flux(data))]
             row += [right_txt(data['fs11'])]
             row += [right_txt(data['fs13'])]
+            row += [center_txt(self.get_run_events(data))]
             row += [center_txt(conv_time(data['starttime0']))]
             row += [self.DiaScans.calc_duration(tc, int(run))]
             for channel in get_dia_channels(data):
@@ -99,10 +103,17 @@ class RunTable(Table):
                 row += [make_abs_link(join('Diamonds', dia, 'index.html'), dia)]
                 row += [make_bias_str(data['dia{}hv'.format(channel)])]
                 if channel == '1' and max_channels != len(get_dia_channels(data)):
-                    row += ['', '']
+                    row += ['', ''] * (max_channels - len(get_dia_channels(data)))
             row += [data['comments'][:100]]
             rows.append(row)
         return add_bkg(HTMLTable.table(rows, header_row=header), self.BkgCol)
+
+    @staticmethod
+    def get_run_events(run_info):
+        if 'events' in run_info:
+            n = run_info['events']
+            return '{:1.1f}M'.format(n / 1e6) if n > 1e6 else '{:1.0f}k'.format(n / 1e3)
+        return '?'
 
 
 if __name__ == '__main__':
