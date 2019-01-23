72dfcf94b2a58c55d336b381d7b7f2a8991aa317
Michael <remichae@phys.ethz.ch>
2019-01-23 15:14:37 +0100

    sort dias in Yeartable and break too long set strings

--- src/YearTable.py
+++ src/YearTable.py
@@ -90,17 +90,19 @@ class YearTable(Table):
         return [dic[col] if col in dic else col for col in self.OtherCols]
 
     def get_single_crystal_dias(self):
-        return [dia for dia in loads(self.Config.get('General', 'single_crystal')) if dia not in self.Exclude]
+        dias = [dia for dia in loads(self.Config.get('General', 'single_crystal')) if dia not in self.Exclude]
+        s_dias = [dia for dia in dias if dia.startswith('S') and dia[2].isdigit()]
+        return sorted(s_dias, key=lambda x: int(remove_letters(x))) + sorted(dia for dia in dias if dia not in s_dias)
 
     def get_si_diodes(self):
-        return [dia for dia in loads(self.Config.get('General', 'si-diodes')) if dia not in self.Exclude]
+        return sorted(dia for dia in loads(self.Config.get('General', 'si-diodes')) if dia not in self.Exclude)
 
     def get_diamond_names(self, scvd=False, si=False):
         if scvd:
             return self.get_single_crystal_dias()
         elif si:
             return self.get_si_diodes()
-        return [dia for dia in self.Diamonds if dia not in self.get_single_crystal_dias() + self.get_si_diodes() + self.Exclude]
+        return sorted([dia for dia in self.Diamonds if dia not in self.get_single_crystal_dias() + self.get_si_diodes() + self.Exclude])
 
     def make_set_string(self, tc, dia):
         dias = self.DiaScans.get_all_rp_diamonds(tc)
@@ -109,8 +111,9 @@ class YearTable(Table):
             if dias[i] != dias[i - 1]:
                 dia_set.append(dias[i])
         strings = ['{i}{j}'.format(i=i, j='f' if not tup.index(dia) else 'b') for i, tup in enumerate(dia_set, 1) if dia in tup]
-        if len(strings) > 3:
-            strings[3] = '<br/>{}'.format(strings[3])
+        for i in xrange(len(strings)):
+            if i and not i % 3:
+                strings[i] = '<br/>{}'.format(strings[i])
         return center_txt(','.join(strings))
 
     def make_info_str(self, tc, dia):
